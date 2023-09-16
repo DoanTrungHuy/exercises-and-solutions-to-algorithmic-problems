252d29f962d6d9ae09db6d4a8263e78db117e72a
doantrunghuy <doantrunghuy1082k2@gmail.com>
2023-09-16 14:39:19 +0700

    DP

--- Contest Codeforces by Colin Galen/Dynamic Programming/L.cpp
+++ Contest Codeforces by Colin Galen/Dynamic Programming/L.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 using ll = long long;
 
-const ll MN = 1e3 + 1;
 ll n, k;
-pair<ll, ll> groups[MN];
-ll r[MN];
-ll memo[MN][MN];
+vector<array<ll, 3>> groups;
+vector<array<ll, 2>> tables;
+const ll MN = 1001;
+ll memo[MN][MN], color[MN][MN];
 const ll INF = 1e18;
-ll par[MN][MN];
 
 ll dp(ll i, ll j) {
     if (i == n or j == k) {
@@ -18,67 +17,58 @@ ll dp(ll i, ll j) {
     if (memo[i][j] != -1) {
         return memo[i][j];
     }
-    ll ans = 0, val = 0;
-    if (groups[i].first <= r[j]) {
-        val = dp(i + 1, j + 1) + groups[i].second;
+    ll ans = -INF, val = 0;
+    ll c = groups[i][0], p = groups[i][1], r = tables[j][0];
+    if (c <= r) {
+        val = dp(i + 1, j + 1) + p;
         if (ans < val) {
+            color[i][j] = 1;
             ans = val;
-            par[i][j] = 1;
         }
     }
-
     val = dp(i + 1, j);
     if (ans < val) {
+        color[i][j] = 2;
         ans = val;
-        par[i][j] = 2;
     }
-
     val = dp(i, j + 1);
     if (ans < val) {
+        color[i][j] = 3;
         ans = val;
-        par[i][j] = 3;
     }
-
     return memo[i][j] = ans;
 }
-
-vector<array<ll, 3>> group_vals;
-vector<array<ll, 2>> table_vals;
 
 int main() {
     cin >> n;
     for (ll i = 0; i < n; ++i) {
-        cin >> groups[i].first >> groups[i].second;
-        group_vals.push_back({groups[i].first, groups[i].second, i});
+        ll c, p;
+        cin >> c >> p;
+        groups.push_back({c, p, i});
     }
-
     cin >> k;
     for (ll i = 0; i < k; ++i) {
-        cin >> r[i];
-        table_vals.push_back({r[i], i});
+        ll r;
+        cin >> r;
+        tables.push_back({r, i});
     }
-
-    sort(groups, groups + n);
-    sort(r, r + k);
-    sort(group_vals.begin(), group_vals.end());
-    sort(table_vals.begin(), table_vals.end());
+    sort(groups.begin(), groups.end());
+    sort(tables.begin(), tables.end());
     memset(memo, -1, sizeof(memo));
-    memset(par, 0, sizeof(par));
-
+    memset(color, 0, sizeof(color));
     dp(0, 0);
     ll curr_group = 0, curr_table = 0;
-    vector<pair<ll, ll>> ops;
-
+    vector<array<ll, 2>> idxs;
     while (curr_group < n or curr_table < k) {
-        if (par[curr_group][curr_table] == 1) {
-            ops.push_back({group_vals[curr_group][2], table_vals[curr_table][1]});
+        if (color[curr_group][curr_table] == 1) {
+            idxs.push_back({groups[curr_group][2], tables[curr_table][1]});
             curr_group++;
             curr_table++;
         }
-        else if (par[curr_group][curr_table] == 2) {
+        else if (color[curr_group][curr_table] == 2) {
             curr_group++;
         }
-        else if (par[curr_group][curr_table] == 3) {
+        else if (color[curr_group][curr_table] == 3) {
             curr_table++;
         }
         else {
@@ -86,14 +76,9 @@ int main() {
             curr_table++;
         }
     }
-
-    ll m = ops.size();
-
-    cout << m << ' ' << memo[0][0] << '\n';
-
-    for (ll i = 0; i < m; ++i) {
-        cout << ops[i].first + 1 << ' ' << ops[i].second + 1 << '\n';
+    cout << idxs.size() << ' ' << memo[0][0] << '\n';
+    for (array<ll, 2> idx : idxs) {
+        cout << idx[0] + 1 << ' ' << idx[1] + 1 << '\n';
     }
-
 	return 0;
 }
